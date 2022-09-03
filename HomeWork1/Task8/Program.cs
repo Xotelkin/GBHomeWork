bd899cfa56d748754f9bdcb43d8af3d5918c10f3
Xotelkin <stefanno666@mail.ru>
2022-09-03 18:50:26 +0200

    Добавил код Задание 8

---
+++ HomeWork1/Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < numberN + 1)
+    {
+        if (i%2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        i++;
+    }
