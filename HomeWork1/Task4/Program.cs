79641e39023332011698e5ac07ceeae08077da15
Xotelkin <stefanno666@mail.ru>
2022-09-03 17:09:00 +0200

    Добавил код Задание 4

---
+++ HomeWork1/Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Max(num1, Math.Max(num2, num3)));
