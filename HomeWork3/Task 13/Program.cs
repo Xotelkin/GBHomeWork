6791e7acb55defc8198d483ff9622ec044e02a73
Xotelkin <stefanno666@mail.ru>
2022-09-11 19:24:32 +0200

    Добавил код Задание 13

---
+++ HomeWork3/Task 13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Palindrome(int num)
+{ 
+    string result = "";
+    if (num > 9999 && num < 100000)
+    {
+        if (num / 10000 == num % 10)
+        {
+            if (num / 1000 - (num / 10000 * 10) == num / 10 % 10)
+                result = "Это палиндром";   
+        }
+        else
+            result = "Это не палиндром";
+    }
+    else
+        result = "Это не пятизначное число";
+    return result;
+}
+
+System.Console.WriteLine(Palindrome(number));
