516d2417e760274c75dad1d7f612b3282612588a
Xotelkin <stefanno666@mail.ru>
2022-09-15 15:03:01 +0200

    Добавил код Задание 29

---
+++ HomeWork4/Task 29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] array = new int[8];
+FillAray(array);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+void FillAray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-1000, 1000);
+}
