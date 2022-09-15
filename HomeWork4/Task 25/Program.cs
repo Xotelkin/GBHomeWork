bdc1d364d0af621a1aa85e100523d1d4aed46688
Xotelkin <stefanno666@mail.ru>
2022-09-15 13:13:17 +0200

    Добавил код Задание 25

---
+++ HomeWork4/Task 25/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+int summ = 1;
+for(int i = 0; i < numberB; i++)
+    summ *= numberA;
+
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {summ}");
