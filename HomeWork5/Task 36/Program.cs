c26b60af9bb60e22de4605458800943feb23777b
Xotelkin <stefanno666@mail.ru>
2022-09-15 20:10:53 +0200

    Добавил код Задание 36

---
+++ HomeWork5/Task 36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int size = 10;
+int [] array = new int[size];
+FillAray(array);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine("");
+Console.WriteLine($"Сумма элементов с нечетным индексом равна: {SummNumberUnvenIndex(array)}");
+
+
+void FillAray(int [] array)
+{
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(1, 10);
+}
+
+int SummNumberUnvenIndex (int [] array)
+{
+    int summ = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 2 == 0)
+            summ += array[i];
+    }
+    return summ;
+}
