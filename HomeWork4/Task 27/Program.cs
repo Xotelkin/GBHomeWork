835eae197bd9f9d40ac3a42125b753db28a482e9
Xotelkin <stefanno666@mail.ru>
2022-09-15 14:24:45 +0200

    Добавил код Задание 27

---
+++ HomeWork4/Task 27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int number = Proverka(num);
+int Summa = Calculation(ProvOtric(number));
+Console.WriteLine(Summa);
+
+int Proverka(int a)
+{
+    if (num < 0)
+        num *= -1;
+    return num;
+}
+
+int Calculation(int number)
+{
+    if (num < 11)
+        return num;
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10; 
+    }
+    return sum;
+}
+
