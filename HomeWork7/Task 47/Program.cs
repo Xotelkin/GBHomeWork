c1a1a27f25ad11c9a56b855125243b2d7de865ea
Xotelkin <stefanno666@mail.ru>
2022-09-25 18:45:38 +0200

    Добавил код Задание 47

---
+++ HomeWork7/Task 47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int m = Promt("Введите количество строк: ");
+int n = Promt("Введите количество столбцов: ");
+double [, ] array = new double [m, n];
+FillArray (array);
+PrintArray (array);
+
+
+void FillArray (double [, ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round(new Random().Next(10, 100) + new Random().NextDouble(), 1);
+    }
+}
+
+
+void PrintArray(double [, ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int Promt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
