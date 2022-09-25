94c737c2eeb60705b26c5c03d702fb498a57f6c1
Xotelkin <stefanno666@mail.ru>
2022-09-25 21:08:25 +0200

    Добавил код Задание 52

---
+++ HomeWork7/Task 52/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int m = new Random().Next(3, 7);
+int n = new Random().Next(3, 7);
+int [, ] array = new int [m, n];
+FillArray (array);
+PrintArray (array);
+ArithmeticMeanColumn (array);
+
+void FillArray (int [, ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(10, 100);
+    }
+}
+
+
+void PrintArray(int [, ] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ArithmeticMeanColumn (int [, ] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++) 
+            {
+                float summ = 0;
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    summ += array[i, j];
+                }
+                Console.WriteLine($"Cреднее арифметическое элементов {j + 1} столбца = {summ / array.GetLength(0)}");
+            }
+}
