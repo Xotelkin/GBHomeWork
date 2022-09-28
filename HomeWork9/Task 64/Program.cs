cebe75b779e61ccdf3cf7f5969b4e0faff234e4b
Xotelkin <stefanno666@mail.ru>
2022-09-28 20:40:55 +0200

    Добавил код Задание 64

---
+++ HomeWork9/Task 64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int n = Promt ("Введите положительное число: ");
+NaturalNumber(n);
+Console.WriteLine(NaturalNumber(n));
+
+
+string NaturalNumber(int num)
+{
+    if (num == 1)
+        return num.ToString();
+    return num.ToString() + ", " + NaturalNumber(num - 1);
+}
+
+int Promt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
